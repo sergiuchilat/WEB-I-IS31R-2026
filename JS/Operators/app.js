ec580935cdd26382da1f1c6a4f646d3512f25541
sergiuchilat <chilatsergiu@gmail.com>
2026-01-12 16:17:24 +0200

    JS Operators

---
+++ JS/Operators/app.js
@@ -0,0 +1,60 @@
+let x = 1;
+let y = 2;
+
+console.log('x =', x);
+console.log('y =', y);
+
+console.log('x = y:', x == y);      // false
+
+
+x = 1;
+y = '1';
+
+console.log('x =', x);
+console.log('y =', y);
+
+console.log('x == y:', x == y);
+console.log('x === y:', x === y);
+
+x = 0;
+y = false;
+
+console.log('x =', x);
+console.log('y =', y);
+
+console.log('x == y:', x == y);
+console.log('x === y:', x === y);
+
+x = 1
+y = 1.0
+
+console.log('x =', x);
+console.log('y =', y);
+
+console.log('x == y:', x == y);
+console.log('x === y:', x === y);
+
+
+x = 1;
+y = '2';
+console.log('x =', x);
+console.log('y =', y);
+console.log('x + y =', x + y);
+
+x = 1;
+y = 2;
+console.log('x =', x);
+console.log('y =', y);
+console.log('x + y =', x + y);
+
+x = 1;
+y = '2';
+console.log('x =', x);
+console.log('y =', y);
+console.log('x - y =', x - y);
+
+x = 1;
+y = '2a';
+console.log('x =', x);
+console.log('y =', y);
+console.log('x - y =', x - y);
