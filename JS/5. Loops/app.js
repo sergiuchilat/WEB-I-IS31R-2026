35a9eb5f0adc2a3b93d5a37ba18a259eb036cc1e
sergiuchilat <chilatsergiu@gmail.com>
2026-01-12 16:29:51 +0200

    JS Loops

---
+++ JS/5. Loops/app.js
@@ -0,0 +1,33 @@
+console.log('1')
+console.log('2')
+console.log('3')
+console.log('4')
+console.log('5')
+console.log('6')
+console.log('7')
+console.log('8')
+console.log('9')
+console.log('10')
+console.log('11')
+
+
+console.log('--------- FOR ---------');
+for(let i = 1; i <= 11; i++) {
+    console.log(i);
+}
+
+console.log('--------- WHILE ---------');
+let i = 1;
+while( i <= 11) {
+    console.log(i);
+    i++
+}
+
+
+console.log('--------- WHILE ---------');
+let i = 1;
+do {
+    console.log(i);
+    i++
+}while( i <= 11)
+
