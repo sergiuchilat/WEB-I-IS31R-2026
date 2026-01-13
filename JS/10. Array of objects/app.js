5a61da5dd63d4282506152bb0b2e4c0889a3b01f
sergiuchilat <chilatsergiu@gmail.com>
2026-01-13 14:32:33 +0200

    Array of objects

---
+++ JS/10. Array of objects/app.js
@@ -0,0 +1,30 @@
+const student = {
+    name: "Susvaliuc Ilia",
+    studyGroup: "IS31R",
+    averageGrade: 8.5
+}
+
+console.log('Student name:', student.name);
+console.log('Study group:', student.studyGroup);
+console.log('Average grade:', student.averageGrade);
+
+const students = [
+    {
+        name: "Susvaliuc Ilia",
+        studyGroup: "IS31R",
+        averageGrade: 8.5
+    },
+    {
+        name: "Doe John",
+        studyGroup: "IS32R",
+        averageGrade: 9.0,
+        hasSalary: true
+    },
+    {
+        name: "Smith Jane",
+        studyGroup: "IS31R",
+        averageGrade: 7.5
+    }
+];
+
+console.log('Second student name:', students[1].name);
