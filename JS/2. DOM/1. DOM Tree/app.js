24172476bdce0a4fb6ff83885f3c88c4c215aa08
sergiuchilat <chilatsergiu@gmail.com>
2026-01-13 15:38:20 +0200

    JS DOM tree

---
+++ JS/2. DOM/1. DOM Tree/app.js
@@ -0,0 +1,4 @@
+document.body.style.backgroundColor = "lightblue";
+document.body.innerHTML += "<h2>Welcome to the DOM Tree example!</h2>";
+
+document.body.innerHTML += "<p>This page demonstrates basic DOM manipulation using JavaScript.</p>";
